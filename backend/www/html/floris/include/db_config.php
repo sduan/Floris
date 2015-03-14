be342579f6634182aa5ce1225daa0c205afad34c
Steve Duan <xdduan@gmail.com>
2015-03-14 03:56:29 +0000

    use Slim framework

---
+++ backend/www/html/floris/include/db_config.php
@@ -0,0 +1,16 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USERNAME', 'floris');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'floris');
+ 
+define('USER_CREATED_SUCCESSFULLY', 0);
+define('USER_CREATE_FAILED', 1);
+define('USER_ALREADY_EXISTED', 2);
+
+?>
